405f936c20ff3a76d787359681c0a1eb3a42d075
eeeprasanna <tjprasannabe@gmail.com>
2020-10-07 13:56:23 +0400

    again modified keys

--- providers.tf
+++ providers.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region = "us-east-1"
-  access_key = "AKIASX6QZVTNWO5WQCQH"
-  secret_key = "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"
+  access_key = "AKIASX6QZVTN24GP6XXV"
+  secret_key = "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"
 }
